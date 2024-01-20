2fb1b7a6153f36bc1bdf9c143d23a798b674004a
Albert Fit <albert.ioan.fit@gmail.com>
2024-01-20 17:39:08 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+In Asteroid Field, the player controls a ship that spawns in the centre of the screen.
+
+The aim of the game is to avoid asteroids that spawn at the top of the screen and move towards the bottom.
+
+Asteroids that reach the bottom convert into points.
+
+If the player hits an asteroid, the ship gains 3 seconds of immunity and one of three lives is lost. 
+
+When all three lives are lost, the end game screen is shown where the final score is displayed. 
---
+++ asteroid_fields.py
@@ -0,0 +1,37 @@
+import sys
+
+import pygame
+
+class AsteroidFields:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+
+        #Initialize te Pygame background settings.
+        pygame.init()
+
+        #Add the clock function to govern the frame rate
+        self.clock = pygame.time.Clock()
+
+        #Create a display window for the graphical elements.
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Asteroid Fields")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+
+        #The loop contains an event loop and manages screen updates.
+        while True:
+            #Monitor keyboard and mouse events.
+            for event in pygame.event.get():
+                #If the user clicks on the window close button:
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #Make the most recently drawn screen visible.
+            pygame.display.flip()
+    
+if __name__ == '__main__':
+    # Make a game instance, and run the game. 
+    af = AsteroidFields()
+    af.run_game()
