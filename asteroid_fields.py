4c896a468c50dea4d69b20bfd56cde9fd30cbd05
Albert Fit <albert.ioan.fit@gmail.com>
2024-01-21 19:28:59 +0000

    Created Background Image

--- asteroid_fields.py
+++ asteroid_fields.py
@@ -30,6 +30,9 @@ class AsteroidFields:
                     sys.exit()
             #Make the most recently drawn screen visible.
             pygame.display.flip()
+
+            #The tick() method takes one argument: the game frame rate
+            self.clock.tick(60)
     
 if __name__ == '__main__':
     # Make a game instance, and run the game. 
